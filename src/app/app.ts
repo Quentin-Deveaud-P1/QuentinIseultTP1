227aa71727fb77f19f8b99eb935353c57e8cb367
Quentin <deveaud.quentin@gmail.com>
2025-09-16 15:14:36 +0200

    TP avec le prof

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Page} from './page/page';
 import {NgStyle} from '@angular/common';
